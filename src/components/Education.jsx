c25f7db5035f84d8f67fddec90466c0909061234
Saahil <saahilmishra016@yahoo.com>
2025-01-12 22:59:55 +0530

    added education with styling

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { motion } from "framer-motion";
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineDot,
+  TimelineConnector,
+  TimelineContent,
+} from "@mui/lab";
 
 const Education = () => {
   const educationList = [
     {
-      institution: "Johnson Grammar School CBSE",
-      degree: "Secondary Education",
-      year: "2020",
+      institution: "Indian Institute of Information Technology, Dharwad",
+      degree: "B.Tech. in Computer Science and Engineering",
+      year: "2026",
     },
     {
       institution: "St. Patrick’s Junior College",
@@ -14,9 +22,9 @@ const Education = () => {
       year: "2022",
     },
     {
-      institution: "Indian Institute of Information Technology, Dharwad",
-      degree: "B.Tech. in Computer Science and Engineering",
-      year: "2026",
+      institution: "Johnson Grammar School CBSE",
+      degree: "Secondary Education",
+      year: "2020",
     },
   ];
 
@@ -30,42 +38,37 @@ const Education = () => {
       >
         Education
       </motion.h2>
-      {educationList.map((item, index) => (
-        <div
-          key={index}
-          className="mb-8 flex flex-col items-start justify-start border-b border-neutral-800 pb-4"
-        >
-          {/* Institution Name */}
-          <motion.h3
-            whileInView={{ opacity: 1, x: 0 }}
-            initial={{ opacity: 0, x: -50 }}
-            transition={{ duration: 0.8 }}
-            className="text-2xl font-bold text-neutral-200 mb-2"
-          >
-            {item.institution}
-          </motion.h3>
-
-          {/* Degree */}
-          <motion.p
-            whileInView={{ opacity: 1, x: 0 }}
-            initial={{ opacity: 0, x: -50 }}
-            transition={{ duration: 1 }}
-            className="text-lg text-neutral-400"
-          >
-            {item.degree}
-          </motion.p>
-
-          {/* Year */}
-          <motion.p
-            whileInView={{ opacity: 1, x: 0 }}
-            initial={{ opacity: 0, x: -50 }}
-            transition={{ duration: 1.2 }}
-            className="text-sm text-neutral-500 mt-1"
-          >
-            {item.year}
-          </motion.p>
-        </div>
-      ))}
+      <Timeline position="alternate">
+        {educationList.map((item, index) => (
+          <TimelineItem key={index}>
+            <TimelineSeparator>
+              <TimelineDot
+                variant="outlined"
+                color={
+                  index === educationList.length - 1 ? "success" : "secondary"
+                }
+                sx={{ width: 28, height: 28 }}
+              />
+              {index !== educationList.length - 1 && (
+                <TimelineConnector sx={{ height: 100 }} />
+              )}
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: 3, px: 2 }}>
+              <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: index % 2 != 0 ? -50 : 50 }} // Alternating motion
+                transition={{ duration: 1 }}
+              >
+                <h3 className="text-3xl font-bold text-neutral-200 mb-2">
+                  {item.institution}
+                </h3>
+                <p className="text-xl text-neutral-400">{item.degree}</p>
+                <p className="text-lg text-neutral-500 mt-1">{item.year}</p>
+              </motion.div>
+            </TimelineContent>
+          </TimelineItem>
+        ))}
+      </Timeline>
     </div>
   );
 };
